6b149d70ec06e0f1c5915395947776f2851017ab
Sami AS <samiabdulsater@gmail.com>
2026-05-21 13:48:24 +0200

    Be more economic with simulations

--- applications/gospel/simulation-cluster.py
+++ applications/gospel/simulation-cluster.py
@@ -41,9 +41,13 @@ app = typer.Typer(add_completion=False)
 
 SAMPLED_BASE = Path("applications/gospel/sampled_circuits")
 ENT_ERRORS: list[float] = list(np.logspace(-6, -1, num=10))
-# Removing the two first
+# Removing the first couple of them
 ENT_ERRORS.pop(0)
 ENT_ERRORS.pop(0)
+ENT_ERRORS.pop(0)
+ENT_ERRORS.pop(0)
+ENT_ERRORS.pop(0)
+print(ENT_ERRORS)
 FOLDER_RE = re.compile(r"^circuits-(\d+)-(\d+)-(.+)$")
 
 
